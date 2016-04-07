2a78f42b138412da933b5d73dfce0eecaffc4054
andreiamtf <andreiamtf@gmail.com>
2016-04-07 01:36:05 +0100

    Added files via upload

---
+++ ViewModel/wholeWeather.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DotNet.Highcharts;
+using DotNet.Highcharts.Helpers;
+using DotNet.Highcharts.Options;
+using MyWeatherAPI.Models.WeatherAPI;
+
+  
+
+namespace MyWeatherAPI.ViewModel
+{
+    public class wholeWeather
+    {
+        public Highcharts charts { get; set; }
+
+        public RootObject rootWeatherAPI {get; set;}
+
+        public Dictionary<string, List<List>> WeatherByDays { get; set;}
+    }
+}
